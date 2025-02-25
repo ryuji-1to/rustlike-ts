6c73e81d36acb87ae9706dba42ce080e8ef5f0f6
ryuji-1to <ryuji0222ss@icloud.com>
2025-02-26 07:51:34 +0900

    test: 🧪 add

---
+++ src/option.test.ts
@@ -0,0 +1,91 @@
+import { describe, expect, test } from "vitest";
+import { None, Some } from "./option";
+
+describe("Option<T>", () => {
+  describe("Some<T>", () => {
+    const some = Some("some");
+    test("isSome", () => {
+      expect(some.isSome()).toBe(true);
+    });
+    test("isNone", () => {
+      expect(some.isNone()).toBe(false);
+    });
+    test("unwrap", () => {
+      expect(some.unwrap()).toBe("some");
+    });
+    test("unwrapOr", () => {
+      expect(some.unwrapOr("or")).toBe("some");
+    });
+    test("unwrapOrElse", () => {
+      expect(some.unwrapOrElse(() => "or")).toBe("some");
+    });
+    test("map", () => {
+      expect(some.map((data) => "mapped " + data).unwrap()).toBe("mapped some");
+    });
+    test("map", () => {
+      expect(some.mapOr("or", (data) => data)).toBe("mapped some");
+    });
+    test("map", () => {
+      expect(
+        some.mapOrElse(
+          () => "or",
+          (data) => data
+        )
+      ).toBe("mapped some");
+    });
+    test("and", () => {
+      expect(some.and(Some("new some")).unwrap()).toBe("new some");
+    });
+    test("or", () => {
+      expect(some.or(Some("new some")).unwrap()).toBe("mapped some");
+    });
+  });
+
+  describe("None", () => {
+    const none = None;
+    test("isSome", () => {
+      expect(none.isSome()).toBe(false);
+    });
+
+    test("isNone", () => {
+      expect(none.isNone()).toBe(true);
+    });
+
+    test("unwrap", () => {
+      expect(() => none.unwrap()).toThrowError("panic!!!");
+    });
+
+    test("unwrapOr", () => {
+      expect(none.unwrapOr(99)).toBe(99);
+    });
+
+    test("unwrapOrElse", () => {
+      expect(none.unwrapOrElse(() => 88)).toBe(88);
+    });
+
+    test("map", () => {
+      expect(none.map((x) => x * 2).isNone()).toBe(true);
+    });
+
+    test("mapOr", () => {
+      expect(none.mapOr(99, (x) => x * 2)).toBe(99);
+    });
+
+    test("mapOrElse", () => {
+      expect(
+        none.mapOrElse(
+          () => 99,
+          (x) => x * 2
+        )
+      ).toBe(99);
+    });
+
+    test("and", () => {
+      expect(none.and(Some(10)).isNone()).toBe(true);
+    });
+
+    test("or", () => {
+      expect(none.or(Some(10)).unwrap()).toBe(10);
+    });
+  });
+});
